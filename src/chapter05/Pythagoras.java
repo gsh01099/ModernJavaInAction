7330fdf20c509cad700bacefa227e480c7fb62ca
jjy <gsh01083@gmail.com>
2022-09-09 16:48:10 +0900

    🎨feat: CHAPTER5 피타고라스 수

--- src/chapter05/Pythagoras.java
+++ src/chapter05/Pythagoras.java
@@ -1,2 +1,23 @@
-package chapter05;public class Pythagoras {
+package chapter05;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Pythagoras {
+
+    public static void 피타고라스수() {
+        Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0).boxed()
+                        .map(b -> new int[] { a, b, (int) Math.sqrt(a * a + b * b) }));
+        pythagoreanTriples.forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+
+        Stream<int[]> pythagoreanTriples2 = IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .mapToObj(b -> new double[]{a, b, Math.sqrt(a * a + b * b)})
+                        .filter(t -> t[2] % 1 == 0))
+                .map(array -> Arrays.stream(array).mapToInt(a -> (int) a).toArray());
+        pythagoreanTriples2.forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+    }
 }
